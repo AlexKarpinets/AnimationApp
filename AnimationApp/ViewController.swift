dd9e0d486657b62deb91c814a60a6c6e1dc06924
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-08-04 14:44:46 +0300

    Add someText

--- AnimationApp/ViewController.swift
+++ AnimationApp/ViewController.swift
@@ -5,15 +5,71 @@
 //  Created by Karpinets Alexander on 03.08.2021.
 //
 
-import UIKit
+import Spring
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var buttonCounter = 1
+    private var animation: Animated!
+    
+    @IBOutlet weak var labelNameAnimation: SpringLabel!
+    @IBOutlet weak var springAnimatedView: SpringView!
+    
+    @IBAction func buttonPressedAction(_ sender: SpringButton) {
+        buttonCounter += 1
+        
+        switch buttonCounter {
+        case 1:
+            labelNameAnimation.text = """
+                Preset: Pop
+                Curve: easeInOut
+"""
+            
+            sender.setTitle("Run Shake", for: .normal)
+            
+            springAnimatedView.animation = "pop"
+            springAnimatedView.force = 2
+            springAnimatedView.duration = 1
+            springAnimatedView.delay = 0
+            springAnimatedView.curve = "easeInOut"
+            springAnimatedView.animate()
+            
+        case 2:
+            labelNameAnimation.text = """
+                Preset: Shake
+                Curve: easeInOut
+"""
+            
+            sender.setTitle("Run Squeeze", for: .normal)
+            
+            springAnimatedView.animation = "shake"
+            springAnimatedView.force = 1
+            springAnimatedView.duration = 0.7
+            springAnimatedView.delay = 0.5
+            springAnimatedView.curve = "easeInOut"
+            springAnimatedView.animate()
+            
+        case 3:
+            labelNameAnimation.text = """
+                Preset: Squeeze
+                Curve: easeInSine
+"""
+            
+            sender.setTitle("Run Pop", for: .normal)
+            
+            springAnimatedView.animation = "squeeze"
+            springAnimatedView.force = 3
+            springAnimatedView.duration = 2
+            springAnimatedView.delay = 1
+            springAnimatedView.curve = "easeInSine"
+            springAnimatedView.animate()
+            
+            buttonCounter = 0
+            
+        default:
+            break
+        }
     }
-
-
 }
+
 
